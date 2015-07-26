090af79116c07e9a893809c95b9fb164bed0c30f
Victor Zhong <victor@victorzhong.com>
2015-07-25 23:01:06 -0700

    fix bugs

--- new_mac.sh
+++ new_mac.sh
@@ -13,7 +13,8 @@ xcode-select --install
 # python
 echo "setting up python"
 brew tap homebrew/python
-brew install python numpy scipy matplotlib theano ipython theano docopt
+brew install python numpy scipy matplotlib 
+pip install theano ipython theano docopt
 
 # shell
 echo "setting up shell environment"
@@ -31,18 +32,19 @@ python install.py
 
 # ruby
 echo "setting up ruby"
+brew install gnupg gnupg2
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 gem install bropages
 
 # java
 echo "setting up java"
-brew install java
+brew cask install java
 
 # brew cask
 echo "setting up additional software via cask"
 brew install caskroom/cask/brew-cask
-brew cask install iterm pycharm-ultimate intellij-ultimate spotify dropbox mailbox 
+brew cask install iterm pycharm intellij-idea spotify dropbox mailbox 
 brew cask install google-drive google-chrome google-hangout flux evernote atom sunrise postgres
 echo "you should install 1password manually"
 
